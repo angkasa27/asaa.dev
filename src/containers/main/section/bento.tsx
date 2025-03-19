e51066f787d76f66886dbed9e942dd133b6ff177
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2025-03-19 11:55:39 +0700

    Update bento layout

--- src/containers/main/section/bento.tsx
+++ src/containers/main/section/bento.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
 import { ClassValue } from "clsx";
 import { ReactNode } from "react";
+import { LuGithub } from "react-icons/lu";
 
 // TODO: Add content
 // TODO: Add custom animation
@@ -8,26 +9,37 @@ import { ReactNode } from "react";
 export const BentoSection = () => {
   return (
     <section id="bento">
-      <div className="grid grid-cols-4 grid-rows-4 gap-2">
-        <BentoCardContainer className="col-span-2 row-span-2">
-          Github
+      <div className="grid grid-cols-12 gap-2 auto-rows-fr">
+        <BentoCardContainer
+          className={cn(
+            "col-span-12 row-span-2 sm:col-span-6 sm:row-span-2",
+            "p-4 flex flex-col"
+          )}
+        >
+          <div className="flex-grow">
+            <LuGithub className="size-6" />
+          </div>
+          <p className="text-lg font-semibold">GitHub</p>
+          <p className="text-sm">All the stuff that i make</p>
         </BentoCardContainer>
-        <BentoCardContainer className="col-span-2 row-span-2">
-          Github2
+        <BentoCardContainer className="col-span-12 row-span-2 sm:col-span-6 sm:row-span-2">
+          <div>
+            
+          </div>
         </BentoCardContainer>
-        <BentoCardContainer className="col-span-1 row-span-2">
+        <BentoCardContainer className="col-span-6 row-start-7 col-start-1 sm:col-span-3 row-span-2 sm:row-start-auto sm:col-start-auto">
           About
         </BentoCardContainer>
-        <BentoCardContainer className="col-span-1 row-span-1">
+        <BentoCardContainer className="col-span-6 sm:col-span-3 sm:row-span-1">
           Linkedin
         </BentoCardContainer>
-        <BentoCardContainer className="col-span-1 row-span-1">
+        <BentoCardContainer className="col-span-6 sm:col-span-3 sm:row-span-1">
           Twitter
         </BentoCardContainer>
-        <BentoCardContainer className="col-span-2 row-span-1">
+        <BentoCardContainer className="col-span-12 sm:col-span-6 sm:row-span-1">
           Leetcode
         </BentoCardContainer>
-        <BentoCardContainer className="col-start-4 row-start-3 col-span-1 row-span-2">
+        <BentoCardContainer className="col-span-6 sm:col-start-10 sm:row-start-3 sm:col-span-3 row-span-2">
           Discord
         </BentoCardContainer>
       </div>
@@ -42,5 +54,14 @@ const BentoCardContainer = ({
   children: ReactNode;
   className?: ClassValue;
 }) => {
-  return <div className={cn("rounded-lg border hover:scale-95 transition-transform duration-200", className)}>{children}</div>;
+  return (
+    <div
+      className={cn(
+        "rounded-lg border hover:scale-95 transition-transform duration-200",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
 };
