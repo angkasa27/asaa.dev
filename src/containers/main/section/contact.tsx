75c28a2f9e3570da1c5e06b63e8832982bbc6287
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2025-03-19 14:23:18 +0700

    Highlight link

--- src/containers/main/section/contact.tsx
+++ src/containers/main/section/contact.tsx
@@ -10,19 +10,19 @@ export const ContactSection = () => {
         Have an exciting project in mind, a brilliant idea you&apos;re eager to
         discuss, or simply want to explore new possibilities within the realm of
         web development and design?
-        <a className="text-white" href="mailto:mas.angkasa27@gmail.com">
+        <a className="text-white underline" href="mailto:mas.angkasa27@gmail.com">
           Drop me an email
         </a>
       </p>{" "}
       <p className="text-pretty font-sans text-sm sm:text-base leading-6 text-muted-foreground text-center">
-        Let&apos;s connect on
+        Let&apos;s connect 
+        {" on "}
         <a
-          className="text-white"
+          className="text-white underline"
           href="https://www.linkedin.com/in/dimasangkasa"
           target="_blank"
           rel="noreferrer"
         >
-          {" "}
           LinkedIn
         </a>{" "}
         to expand our networks and discover how we can grow together
