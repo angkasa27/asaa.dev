a504c764e753ee80dbbef768e61f25034fc7ed37
Dimas Angkasa Nurindra <hadiaririnpancawati@gmail.com>
2025-04-19 12:31:01 +0700

    Update Highlighted project

--- src/containers/main/section/project.tsx
+++ src/containers/main/section/project.tsx
@@ -26,6 +26,18 @@ export const ProjectSection = () => {
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-10">
         <ProjectCard
+          title="NeuCentrIX"
+          description="Create a web-based platform providing online invitation services for weddings, events, and other special moments. Designed to offer a modern and elegant solution, Surat Bahagia helps users create and share personalized invitations easily. "
+          stack={["React", "Tailwind CSS", "NextJS"]}
+          projectUrl="https://www.neucentrix.co.id/en/"
+        />
+        <ProjectCard
+          title="Dwipatelco"
+          description="Create a website showcasing GitHub repository projects for the Moklet community, a group consisting of students, teachers, and alumni of SMK Telkom Malang."
+          stack={["React", "Tailwind CSS"]}
+          projectUrl="https://dwipatelco.co.id/"
+        />{" "}
+        <ProjectCard
           title="SuratBahagia"
           description="Create a web-based platform providing online invitation services for weddings, events, and other special moments. Designed to offer a modern and elegant solution, Surat Bahagia helps users create and share personalized invitations easily. "
           stack={["React", "Tailwind CSS", "NextJS"]}
