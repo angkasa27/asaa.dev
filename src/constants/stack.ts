bade67519f8d04362c4f7a5037e6a8db2694eca0
Dimas Angkasa Nurindra <mas.angkasa27@gmail.com>
2025-08-16 12:24:16 +0700

    Refactor stack implementation to consolidate frontend, backend, and app stacks into a single constant; remove unused imports and components.

---
+++ src/constants/stack.ts
@@ -0,0 +1,205 @@
+import { IconType } from "react-icons";
+// import { FaJava } from "react-icons/fa6";
+import {
+  SiCloudflare,
+  SiExpress,
+  SiFigma,
+  SiGithub,
+  SiGithubcopilot,
+  // SiJavascript,
+  SiJira,
+  SiJsonwebtokens,
+  SiMongodb,
+  SiMui,
+  SiMysql,
+  SiNestjs,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiOpenai,
+  SiPostgresql,
+  SiPostman,
+  // SiPython,
+  SiRadixui,
+  SiReact,
+  SiShadcnui,
+  SiTailwindcss,
+  // SiTypescript,
+} from "react-icons/si";
+import { VscVscode } from "react-icons/vsc";
+
+export interface Stack {
+  icon: IconType;
+  name: string;
+}
+
+const FRONTEND_STACK = [
+  {
+    icon: SiReact,
+    name: "React",
+  },
+  {
+    icon: SiNextdotjs,
+    name: "Next.js",
+  },
+  {
+    icon: SiTailwindcss,
+    name: "Tailwind CSS",
+  },
+  {
+    icon: SiShadcnui,
+    name: "Shadcn UI",
+  },
+  {
+    icon: SiMui,
+    name: "Material UI",
+  },
+  {
+    icon: SiRadixui,
+    name: "Radix UI",
+  },
+];
+
+const BACKEND_STACK = [
+  {
+    icon: SiNestjs,
+    name: "NestJS",
+  },
+  {
+    icon: SiNodedotjs,
+    name: "Node.js",
+  },
+  {
+    icon: SiExpress,
+    name: "Express",
+  },
+  {
+    icon: SiMysql,
+    name: "MySQL",
+  },
+  {
+    icon: SiPostgresql,
+    name: "PostgreSQL",
+  },
+  {
+    icon: SiJsonwebtokens,
+    name: "JSON Web Tokens",
+  },
+  {
+    icon: SiMongodb,
+    name: "MongoDB",
+  },
+];
+
+// const LANGUAGE_STACK = [
+//   {
+//     icon: FaJava,
+//     name: "Java",
+//   },
+//   {
+//     icon: SiJavascript,
+//     name: "JavaScript",
+//   },
+//   {
+//     icon: SiPython,
+//     name: "Python",
+//   },
+//   {
+//     icon: SiTypescript,
+//     name: "TypeScript",
+//   },
+
+//   // Repeated
+
+//   {
+//     icon: FaJava,
+//     name: "Java",
+//   },
+//   {
+//     icon: SiJavascript,
+//     name: "JavaScript",
+//   },
+//   {
+//     icon: SiPython,
+//     name: "Python",
+//   },
+//   {
+//     icon: SiTypescript,
+//     name: "TypeScript",
+//   },
+// ];
+
+// const LIBRARY_STACK = [
+//   {
+//     icon: SiReact,
+//     name: "React",
+//   },
+//   {
+//     icon: SiNextdotjs,
+//     name: "Next.js",
+//   },
+//   {
+//     icon: SiTailwindcss,
+//     name: "Tailwind CSS",
+//   },
+//   {
+//     icon: SiShadcnui,
+//     name: "Shadcn UI",
+//   },
+//   {
+//     icon: SiMui,
+//     name: "Material UI",
+//   },
+//   {
+//     icon: SiRadixui,
+//     name: "Radix UI",
+//   },
+//   {
+//     icon: SiNestjs,
+//     name: "NestJS",
+//   },
+//   {
+//     icon: SiNodedotjs,
+//     name: "Node.js",
+//   },
+//   {
+//     icon: SiExpress,
+//     name: "Express",
+//   },
+// ];
+
+const APP_STACK = [
+  {
+    icon: SiFigma,
+    name: "Figma",
+  },
+  {
+    icon: SiGithub,
+    name: "Github",
+  },
+  {
+    icon: SiPostman,
+    name: "Postman",
+  },
+  {
+    icon: SiOpenai,
+    name: "ChatGPT",
+  },
+  {
+    icon: SiJira,
+    name: "Jira",
+  },
+  {
+    icon: SiGithubcopilot,
+    name: "Github Copilot",
+  },
+  {
+    icon: VscVscode,
+    name: "VS Code",
+  },
+  {
+    icon: SiCloudflare,
+    name: "Cloudflare",
+  },
+];
+
+export const STACK = [FRONTEND_STACK, BACKEND_STACK, APP_STACK];
--- src/containers/main/section/stack.tsx
+++ src/containers/main/section/stack.tsx
@@ -1,23 +1,5 @@
 import { InfiniteSlider } from "@/components/fragments/infinite-slider/infinite-slider";
-import { FaJava } from "react-icons/fa6";
-import {
-  SiFigma,
-  SiGithub,
-  SiGithubcopilot,
-  SiJavascript,
-  SiJira,
-  SiMui,
-  SiNextdotjs,
-  SiOpenai,
-  SiPostman,
-  SiPython,
-  SiRadixui,
-  SiReact,
-  SiShadcnui,
-  SiTailwindcss,
-  SiTypescript,
-} from "react-icons/si";
-import { VscVscode } from "react-icons/vsc";
+import { STACK } from "@/constants/stack";
 
 // TODO: Change stack to colored icon
 // TODO: Change stack to front end, back end, and complementary tools
@@ -26,137 +8,27 @@ export const StackSection = () => {
   return (
     <section id="stack">
       <div className="relative max-sm:-mx-6">
-        <InfiniteSlider speedOnHover={10} speed={40}>
-          {LANGUAGE_STACK.map((stack, i) => (
-            <div
-              key={i}
-              className="flex w-max items-center gap-2 rounded-full border border-neutral-300 bg-neutral-50 px-5 py-2 text-[15px] shadow-sm dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-50 mb-4"
-            >
-              <stack.icon />
-              <p>{stack.name}</p>
-            </div>
-          ))}
-        </InfiniteSlider>{" "}
-        <InfiniteSlider speedOnHover={10} speed={35} reverse>
-          {LIBRARY_STACK.map((stack, i) => (
-            <div
-              key={i}
-              className="flex w-max items-center gap-2 rounded-full border border-neutral-300 bg-neutral-50 px-5 py-2 text-[15px] shadow-sm dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-50 mb-4"
-            >
-              <stack.icon />
-              <p>{stack.name}</p>
-            </div>
-          ))}
-        </InfiniteSlider>{" "}
-        <InfiniteSlider speedOnHover={10} speed={30}>
-          {APP_STACK.map((stack, i) => (
-            <div
-              key={i}
-              className="flex w-max items-center gap-2 rounded-full border border-neutral-300 bg-neutral-50 px-5 py-2 text-[15px] shadow-sm dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-50"
-            >
-              <stack.icon />
-              <p>{stack.name}</p>
-            </div>
-          ))}
-        </InfiniteSlider>
+        {STACK.map((stack, index) => (
+          <InfiniteSlider
+            key={index}
+            speedOnHover={10}
+            speed={40 - index * 5}
+            reverse={index % 2 === 1}
+          >
+            {stack.map((item, i) => (
+              <div
+                key={i}
+                className="flex w-max items-center gap-2 rounded-full border border-neutral-300 bg-neutral-50 px-5 py-2 text-[15px] shadow-sm dark:border-neutral-700 dark:bg-neutral-800 dark:text-neutral-50 mb-4"
+              >
+                <item.icon />
+                <p>{item.name}</p>
+              </div>
+            ))}
+          </InfiniteSlider>
+        ))}{" "}
         <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background"></div>
         <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background"></div>
       </div>
     </section>
   );
 };
-
-const LANGUAGE_STACK = [
-  {
-    icon: FaJava,
-    name: "Java",
-  },
-  {
-    icon: SiJavascript,
-    name: "JavaScript",
-  },
-  {
-    icon: SiPython,
-    name: "Python",
-  },
-  {
-    icon: SiTypescript,
-    name: "TypeScript",
-  },
-
-  // Repeated
-
-  {
-    icon: FaJava,
-    name: "Java",
-  },
-  {
-    icon: SiJavascript,
-    name: "JavaScript",
-  },
-  {
-    icon: SiPython,
-    name: "Python",
-  },
-  {
-    icon: SiTypescript,
-    name: "TypeScript",
-  },
-];
-
-const LIBRARY_STACK = [
-  {
-    icon: SiReact,
-    name: "React",
-  },
-  {
-    icon: SiNextdotjs,
-    name: "Next.js",
-  },
-  {
-    icon: SiTailwindcss,
-    name: "Tailwind CSS",
-  },
-  {
-    icon: SiShadcnui,
-    name: "Shadcn UI",
-  },
-  {
-    icon: SiMui,
-    name: "Material UI",
-  },
-  {
-    icon: SiRadixui,
-    name: "Radix UI",
-  },
-];
-const APP_STACK = [
-  {
-    icon: SiFigma,
-    name: "Figma",
-  },
-  {
-    icon: SiGithub,
-    name: "Github",
-  },
-  {
-    icon: SiPostman,
-    name: "Postman",
-  },
-  {
-    icon: SiOpenai,
-    name: "ChatGPT",
-  },
-  {
-    icon: SiJira,
-    name: "Jira",
-  },
-  {
-    icon: SiGithubcopilot,
-    name: "Github Copilot",
-  },
-  {
-    icon: VscVscode,
-    name: "VS Code",
-  },
-];
