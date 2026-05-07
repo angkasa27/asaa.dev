3a8e2a3237cb7cd975c16320d568b1851e70c67b
Dimas Angkasa Nurindra <mas.angkasa27@gmail.com>
2026-05-07 09:36:17 +0700

    fix: update About link to include absolute path

--- src/components/navigation/BaseNavigation.svelte
+++ src/components/navigation/BaseNavigation.svelte
@@ -18,7 +18,7 @@
 		</a>
 	</ButtonIcon>
   <div class="flex items-center gap-4">
-    <a href="#about">About</a>
+    <a href="/#about">About</a>
     <a href="/projects">Projects</a>
     <a href="#contact">Contact</a>
   </div>
